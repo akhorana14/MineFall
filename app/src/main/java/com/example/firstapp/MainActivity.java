fc9aa23a68ce9bb2a349a9d1cb8cde52947945d9
akhorana14 <31749972+akhorana14@users.noreply.github.com>
2020-09-10 23:51:04 -0400

    Error: titleScreen component not syncing Working on opacity changing Start button.

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -7,16 +7,47 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    boolean startIsPressed = false;
+    View start = findViewById(R.id.startWord);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startScreen();
+    }
+
+    public void startScreen()
+    {
+
+
+        int opacity = 100;
+        while(!startIsPressed)
+        {
+            if(opacity == 100)
+            {
+                for(opacity = 100; opacity > 0; opacity--)
+                {
+                    start.setAlpha(opacity);
+                }
+            }
+            else if(opacity == 0)
+            {
+                for(opacity = 0; opacity < 100; opacity++)
+                {
+                    start.setAlpha(opacity);
+                }
+            }
+
+        }
+
     }
 
 
 
     public void onStartTap(View view) {
+        startIsPressed = true;
         view.setVisibility(View.INVISIBLE); // Sets the button as invisible
+        start.setVisibility(View.INVISIBLE);
         Game instanceGame = new Game(); //Creates a new instance of the Game class
         instanceGame.start(); //Starts the game
     }
